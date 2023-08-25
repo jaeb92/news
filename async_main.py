f2240e6edc875247b1da42391b91964b992dd2cc
jaeb <jaebin9274@gmail.com>
2023-08-25 16:17:42 +0900

    create asyncio crawler

---
+++ async_main.py
@@ -0,0 +1,101 @@
+import time
+import yaml
+import aiohttp
+import aiofiles
+import asyncio
+from bs4 import BeautifulSoup
+from db.dbconn import Database
+
+main_url = 'https://hankookilbo.com'
+main_category = "Politics"
+subcategories = ["HA01","HA02","HA03","HA04","HA99"]
+db_columns = ['title', 'contents', 'author', 'date', 'source']
+baseurl = main_url + '/News/{category}/{subcategory}'
+
+db = Database()
+
+async def save(news):
+    table = 'news'
+    columns = ','.join(db_columns)
+    sql = f"insert into {table} ({columns}) values %s"
+    values = [tuple(n.values()) for n in news]
+
+
+    # print(values)
+    db.insert_bulk(q=sql, arg=values)
+    
+    
+async def get_html(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers={'User-Agent':'Mozilla/5.0'}) as resp:
+            html_text = await resp.text()
+            html = BeautifulSoup(html_text, 'html.parser')
+            return html
+        
+        
+async def get_detail_url(html):
+    list_tag = 'ul'
+    list_class = 'board-list column-3'
+    news_list = html.find(list_tag, list_class)
+    
+    list_title_tag = 'h3'
+    list_title_class = ''
+    
+    news_titles = news_list.find_all(list_title_tag, list_title_class)
+    
+    detail_url_tag = 'a'
+    detail_url_tags = [tag.find(detail_url_tag) for tag in news_titles]
+
+    detail_urls = [tag.attrs['href'] for tag in detail_url_tags if tag]
+    detail_urls = [main_url + detail_url for detail_url in detail_urls]
+
+    return detail_urls
+    
+    
+async def get_news(url: str):
+    title = ''
+    author = ''
+    date = ''
+    contents = ''
+    source = 'hankook'
+    
+    news_list_html = await get_html(url)
+    news_detail_urls = await get_detail_url(news_list_html)
+    detail_html = (await get_html(url) for url in news_detail_urls)
+
+    news = []
+    
+    async for html in detail_html:
+        title = html.find('h2', 'title').text
+        author = html.find('span', 'nm').text
+        date = html.find('dl', 'wrt-text').text
+        contents = ' '.join([tag.text for tag in html.find_all('p', 'editor-p')])
+        
+        news.append({
+            'title': title.strip(),
+            'author': author.strip(),
+            'date': date.strip(),
+            'contents': contents.strip(),
+            'site': source
+        })
+        
+    await save(news) 
+
+        # print(i, title.strip(), author.strip(), date.strip(), contents.strip()[:10])
+
+    # return news
+    
+    
+async def main():
+    futures = [asyncio.ensure_future(get_news(baseurl.format(category=main_category, subcategory=subcategory))) for subcategory in subcategories]
+    await asyncio.gather(*futures)
+
+    # print(len(news))
+    
+if __name__ == '__main__':
+    start = time.time()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
+    end = time.time()
+    print(f"running time: {end - start:.4f}s")
